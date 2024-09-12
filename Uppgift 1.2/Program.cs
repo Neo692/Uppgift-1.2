3e95bd2c94b6c35cb1a248c77c7ea1608f32833e
nehy06001 <nehy06001@E-PW05CGQV>
2024-09-12 14:40:12 +0200

    Add project files.

---
+++ Uppgift 1.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace uppgift_2
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Windows 11 har kraschat");
+        }
+    }
+}
